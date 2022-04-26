ca0f3e98f0655b8542b3f087d71acc01d6859494
Melissa Longenberger <apple@Irish-iMac.hitronhub.home>
2022-04-26 19:08:08 -0400

    saving back to empty main

--- officeHour/server.py
+++
@@ -1,5 +0,0 @@
-from app import app
-from app.controllers import public, private   # rename the imported files to what ever you name your controller files
-
-if __name__ ==  "__main__":
-    app.run(debug=True)
